ae507b47a461ec351022ce2b77f400bcc54e2f41
Harsh Kachhia <96410794+HarshKachhia@users.noreply.github.com>
2024-10-02 11:27:04 -0400

    Add files via upload
    career fair update

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["4th Year Engineering Student at OntarioTechU","Ex- Jr. Mechatronics Engineering Specalist","SOLIDWORKS Mechanical Design Associate Certification Holder","Yellow Belt Lean Six Sigma Certification Holder" , "3D Modeling/Printing Enthusiast"],
-        typeSpeed: 60,
+        strings: ["Mechatronics Engineering Student at OntarioTech U", "Ex-Tesla Intern", "Embedded Systems Enthusiast", "3D Modeling and Printing Hobbyist"],
+        typeSpeed: 80,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Smart Worker","Problem Solver", "Fast Learner", "Critical Thinker", "Team Player"],
+        strings: ["Problem Solver", "Critical Thinker", "Team Player"],
         typeSpeed: 60,
         backSpeed: 60,
         loop: true
@@ -52,24 +52,25 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
-        nav:true,
+        nav: true,
+        dots: true,
         navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         /* NAV Bars for future update
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         */
         responsive: {
-            0:{
+            0: {
                 items: 1,
-                
+
             },
-            600:{
+            600: {
                 items: 2,
-                
+
             },
-            1000:{
-                items: 3,
-                
+            1000: {
+                items: 2,
+
             }
         }
     });
